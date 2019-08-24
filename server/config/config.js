a00e4fd503af3df37fe852708a56b57e5e86c752
Ashish Kumar <akumar46@buffalo.edu>
2019-08-23 23:02:51 -0400

    changes

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,6 @@ if(env === 'development' || env === "test"){
   var config = require('./config.json');
 
   var envConfig = config[env];
-  console.log(envConfig);
 
   Object.keys(envConfig).forEach((key) => {
     process.env[key] = envConfig[key]
